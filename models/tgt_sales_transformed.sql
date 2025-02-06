583ab5540108c72dad18d3d839cd987c6d08110d
neha singhal <ankit706286@gmail.com>
2025-02-06 05:37:22 +0000

    first changed

---
+++ models/tgt_sales_transformed.sql
@@ -0,0 +1,17 @@
+WITH sales_data AS (
+    SELECT 
+        t.region, 
+        t.sales, 
+        t.currency, 
+        c.rate_to_usd, 
+        t.sales * c.rate_to_usd AS sales_in_usd
+    FROM TGT t
+    LEFT JOIN CURRENCY_RATES c
+    ON t.currency = c.currency
+)
+
+SELECT 
+    region, 
+    SUM(sales_in_usd) AS total_sales_usd
+FROM sales_data
+GROUP BY region;
